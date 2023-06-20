27bd9d0a7d3bfeb528b42caa58d91ccde1723573
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-06-20 23:43:31 +0600

    Cell style update and functionality add

--- src/admin/Components/AllQuizzesComponents/QuizTableCell.tsx
+++ src/admin/Components/AllQuizzesComponents/QuizTableCell.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import TableCell from "@mui/material/TableCell";
+import axios from "axios";
 
 interface Column {
   id: "number" | "title" | "description" | "id" | "short_code";
@@ -12,22 +13,60 @@ interface Column {
 interface QuizTableCell {
   column: Column;
   value: any;
-  deleteQuiz?: any;
   row_id?: any;
+  hover?: any;
+  setRefresh?: any;
+  active: boolean;
+  setActive?: any;
 }
 
-export default function QuizTableCell({ column, value,  deleteQuiz, row_id }: QuizTableCell) {
-  const [copied, setCopied] = React.useState(false);
+export default function QuizTableCell({
+  column,
+  value,
+  row_id,
+  hover,
+  setRefresh,
+  active,
+  setActive,
+}: QuizTableCell) {
+  const [copied, setCopied] = useState(false);
+
+  const home_url = (window as any).userLocalize.home_url;
+
+  function deleteQuiz(id: string) {
+    axios
+      .delete(home_url + `/wp-json/quizbit/v1/quiz/delete/${id}`)
+      .then((response) => {
+        console.log(response);
+        setRefresh(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function editQuiz(id: string) {
+    axios
+      .get(home_url + `/wp-json/quizbit/v1/quiz/edit/${id}`)
+      .then((response) => {
+        console.log(response);
+        setRefresh(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
-    <TableCell key={column.id} align={column.align}>
-      {column.format && typeof value === "number" ? (
-        column.format(value)
-      ) : column.id === "short_code" ? (
-        <>
+    <>
+      <TableCell key={column.id} align={column.align}>
+        {column.format && typeof value === "number" ? (
+          column.format(value)
+        ) : column.id === "short_code" ? (
           <span
             style={{
               cursor: "pointer",
-              backgroundColor: "#dcdcde",
+              backgroundColor: "#E4E6E8",
               padding: "8px 10px",
               borderRadius: "2px",
               transition: "opacity 0.3s",
@@ -46,27 +85,64 @@ export default function QuizTableCell({ column, value,  deleteQuiz, row_id }: Qu
           >
             {value}
           </span>
-          {/* {copied && (
-            <span style={{ color: "gray", marginLeft: "5px" }}>Copied</span>
-          )} */}
-        </>
-      ) : (
-        value
-      )}
+        ) : column.id === "title" ? (
+          <div className="flex flex-row gap-3">
+            <div className="font-medium">{value}</div>
+          </div>
+        ) : (
+          value
+        )}
 
-      {column.id === "title" && (
-        <>
-          <br />
-          <button
-            className="text-[#ff3232] font-bold"
-            onClick={() => {
-              deleteQuiz(row_id);
-            }}
-          >
-            Delete
-          </button>
-        </>
-      )}
-    </TableCell>
+        {column.id === "title" && (
+          <>
+            <div
+              className={
+                column.id !== "title"
+                  ? "hidden"
+                  : "mt-3 flex flex-row gap-3 items"
+              }
+            >
+              <button
+                className="text-blue-400 text-[0.85rem]"
+                onClick={() => {
+                  editQuiz(row_id);
+                }}
+              >
+                Edit
+              </button>
+              |
+              <button
+                className="text-red-400 text-[0.85rem]"
+                onClick={() => {
+                  deleteQuiz(row_id);
+                }}
+              >
+                Delete
+              </button>
+              |
+              <label className="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked={active}
+                  onChange={() => {
+                    setActive(!active);
+                  }}
+                />
+                <div className="w-10 h-5 bg-gray-300 rounded-full  peer-focus:ring-blue-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[5px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-[#864DB6]"></div>
+                <span
+                  className={`ml-3 text-sm font-bold ${
+                    active ? "text-green-500" : "text-gray-500"
+                  }`}
+                >
+                  {active ? "Active" : "Inactive"}
+                </span>
+              </label>
+            </div>
+          </>
+        )}
+      </TableCell>
+    </>
   );
 }
