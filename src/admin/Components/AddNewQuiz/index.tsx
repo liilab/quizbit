e08305c3e7736f3473a2d8e5eaae9764447e0666
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-06-20 23:44:08 +0600

    Input path Update

--- src/admin/Components/AddNewQuiz/index.tsx
+++ src/admin/Components/AddNewQuiz/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Input from "../Shared/Input";
+import Input from "../../../Shared/Input";
 import NewQuiz from "./NewQuiz";
 import axios from "axios";
 
